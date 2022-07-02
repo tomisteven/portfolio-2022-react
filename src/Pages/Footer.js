29f3b7d3b8eb6ccf32b0e0ac8cc7770ff5506512
Tomas Steven <totosteven65@gmail.com>
2022-07-01 23:57:00 -0300

    proyects personales

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -9,9 +9,15 @@ export default function Footer() {
   return (
     <div className='container-footer'>
         <div className='footer'>
+          <a target="_blank" rel='noreferrer' href="https://github.com/tomisteven">
             <img src={github} alt=""/>
+          </a>
+          
             <img src={twiter} alt=""/>
+         
+          <a target="_blank" rel='noreferrer' href="https://www.linkedin.com/in/tomas-steven-a5b83b20b/">
             <img src={linkedin} alt=""/>
+          </a>
             
         </div>    
     </div>
